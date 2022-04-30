6cd347d81b7a06cb9eebd01a21103a97fd57210e
MatheusMiranda96 <matth.miranda@gmail.com>
2022-04-30 11:48:41 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { DialogComponent } from './dialog/dialog.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular13Crud';
+
+  constructor(private dialog: MatDialog) {
+
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width:'30%'
+    });
+  }
+}
---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent implements OnInit {
+
+  freshnessList = ['Novo', 'Usado', 'Remanufaturado'];
+  productForm !: FormGroup  ;
+
+  constructor ( private formBuilder: FormBuilder  ) { }
+
+  ngOnInit(): void {
+       this.productForm = this.formBuilder.group({
+       productName : [' ', Validators.required],
+       category : [' ', Validators.required],
+       freshness : [' ', Validators.required],
+       price : [' ', Validators.required],
+       comment : [' ', Validators.required],
+       date : [' ', Validators.required]
+    }) 
+  }
+
+  addProduct(){
+    console.log(this.productForm.value)
+  }
+}
