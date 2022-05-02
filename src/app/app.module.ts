f1a84a223441661235d0a50ebefc014c8c09c555
MatheusMiranda96 <matth.miranda@gmail.com>
2022-05-02 13:48:33 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatRadioModule } from '@angular/material/radio';
-
+import { MatSortModule } from '@angular/material/sort';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DialogComponent } from './dialog/dialog.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -40,6 +43,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatRadioModule,
     ReactiveFormsModule,
     FormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,31 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog'
 
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.scss']
+  styleUrls: ['./dialog.component.scss'],
 })
 export class DialogComponent implements OnInit {
-
   freshnessList = ['Novo', 'Usado', 'Remanufaturado'];
-  productForm !: FormGroup  ;
+  productForm!: FormGroup;
 
-  constructor ( private formBuilder: FormBuilder  ) { }
+  constructor(private formBuilder: FormBuilder, private api: ApiService, private dialogRef: MatDialogRef<DialogComponent>) {}
 
   ngOnInit(): void {
-       this.productForm = this.formBuilder.group({
-       productName : [' ', Validators.required],
-       category : [' ', Validators.required],
-       freshness : [' ', Validators.required],
-       price : [' ', Validators.required],
-       comment : [' ', Validators.required],
-       date : [' ', Validators.required]
-    }) 
+    this.productForm = this.formBuilder.group({
+      productName: [' ', Validators.required],
+      category: [' ', Validators.required],
+      freshness: [' ', Validators.required],
+      price: [' ', Validators.required],
+      comment: [' ', Validators.required],
+      date: [' ', Validators.required],
+    })
   }
-
-  addProduct(){
-    console.log(this.productForm.value)
+  addProduct() {
+    if (this.productForm.valid) {
+      this.api.postProduct(this.productForm.value).subscribe({
+        next: (res) => {
+          alert('Product added successfully');
+          this.productForm.reset();
+          this.dialogRef.close('save');
+        },
+        error: () => {
+          alert("Error while adding the product")
+        }
+      });
+    }
   }
 }
